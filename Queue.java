c96b9a536ece07dab0825997e536f5a26efaaf15
Priyam Upadhyay <priyamupadhyay17@msitprogram.net>
2022-05-13 10:51:30 +0530

    implemented size

--- Queue.java
+++ Queue.java
@@ -41,7 +41,7 @@ class Queue {
 
     // Return the number of elements in queue.
     public int size() {
-        
+        return obj1.size();
     }
     
     public static void main(String[] args) {
